85d335653ef3a67fa10c7d934d44ad8389a5cd5e
traviscult <traviscult@gmail.com>
2020-04-28 18:37:10 -0400

    added routes

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+const apiRoutes = require("./Routes/apiRoutes")
+const htmlRoutes = require("./Routes/htmlRoutes.js")
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+app.use("/", htmlRoutes)
+
+app.use("/api", apiRoutes )
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`The app is listening on port: ${PORT}`)
+})
