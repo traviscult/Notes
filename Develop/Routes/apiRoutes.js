2c9d7e909815b1957b22668ac4f99fe603a5e08a
traviscult <traviscult@gmail.com>
2020-04-29 16:44:29 -0400

    added note to db json

--- Develop/Routes/apiRoutes.js
+++ Develop/Routes/apiRoutes.js
@@ -1,13 +1,30 @@
-// const fs = require('fs');
-const notes = require()
+const fs = require('fs');
+const util = require('util');
 const router = require('express').Router();
 
 router.get('/notes', (req, res) =>{
-    res.json(notes)
+    // let createdNote = req.params.notes;
+    // console.log(createdNote);
+    // res.json(createdNote)
+
+    fs.readFile("db/db.json", (err, data) => {
+        res.write(data);
+        return res.end();
+    })
 }) 
 
 router.post('/notes', (req, res) =>{
-    res.json(notes)
+    let newNote = req.body; 
+
+    console.log (newNote);
+
+    res.json(newNote)
+
+    fs.appendFile("db/db.json", JSON.stringify(newNote), (err) => {
+        if(err) throw (err);
+        console.log("note saved")
+
+    } )   
 })
 
 router.delete('/notes/:id', (req, res) =>{
---
+++ Develop/db/store.js
@@ -0,0 +1,18 @@
+// const fs = require('fs');
+
+class Store {
+    constructor(){
+
+    }
+    read(){
+
+    }
+
+    write(note){
+
+    }
+
+    getNotes(){
+        
+    }
+}
