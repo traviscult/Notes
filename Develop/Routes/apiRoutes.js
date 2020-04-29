7b62a7982daf1ddff9201cb90f08cf593234018c
traviscult <traviscult@gmail.com>
2020-04-29 15:18:44 -0400

    added api route functions

--- Develop/Routes/apiRoutes.js
+++ Develop/Routes/apiRoutes.js
@@ -1,3 +1,17 @@
 // const fs = require('fs');
+const notes = require()
 const router = require('express').Router();
 
+router.get('/notes', (req, res) =>{
+    res.json(notes)
+}) 
+
+router.post('/notes', (req, res) =>{
+    res.json(notes)
+})
+
+router.delete('/notes/:id', (req, res) =>{
+    res.json(notes)
+})
+
+module.exports = router
